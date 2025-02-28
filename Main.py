3c0b668ab10d5bc5a48e96d7dd4391cf812bc540
Tukaev-Azat <tukaev.azat.a@gmail.com>
2025-02-28 20:55:45 +0300

    Punto не транслитерирует, но остальное делает

--- Main.py
+++ Main.py
@@ -1,20 +1,42 @@
+import time
 import keyboard
 import time
 import threading
 import tkinter as tk
 from tkinter import messagebox
+import pyautogui
 import pyperclip
-
+from pywinauto import Application
+from Functions.func_messages import *
+    
 def get_selected_text():
+    
     """Получает выделенный текст из активного окна."""
+    #if is_editable():
     try:
-        # Копируем выделенный текст в буфер обмена
+        ## Копируем выделенный текст в буфер обмена
+        #pyautogui.hotkey('ctrl', 'c')  # Копируем выделенный текст
+        #time.sleep(0.05)  # Небольшая задержка, чтобы текст успел скопироваться
+        ##pyautogui.typewrite(replace_characters(pyperclip.paste()))  # Вставляем 'x' вместо выделенного текста
+        #old_txt = pyperclip.paste()
+        #new_txt = replace_characters( old_txt )
+        #pyautogui.typewrite( new_txt )  # Вставляем 'x' вместо выделенного текста
+
+        # Заменяем выделенный текст на 'x'
+        #pyautogui.hotkey('ctrl', 'v')  # Вставляем 'x' вместо выделенного текста
+        
+        
         pyperclip.copy("")  # Очищаем буфер обмена
         keyboard.press_and_release('ctrl+c')  # Копируем выделенный текст
         time.sleep(0.05)  # Небольшая задержка, чтобы текст успел скопироваться
-        selected_text = pyperclip.paste()  # Получаем текст из буфера обмена
-        return selected_text
-    except pyperclip.PyperclipException:
+        old_txt = pyperclip.paste()
+        new_txt = replace_characters( old_txt )
+        #pyautogui.typewrite( new_txt )  # Вставляем 'x' вместо выделенного текста
+        pyautogui.typewrite( 'new_txt' )  # Вставляем 'x' вместо выделенного текста
+        time.sleep(0.05)  # Небольшая задержка, чтобы текст успел скопироваться
+        #keyboard.press_and_release( 'ctrl+v' )  # Вставляем 'x' вместо выделенного текста
+               
+    except pyautogui.PyAutoGUIException:
         return ""  # Возвращаем пустую строку, если pyperclip не работает
     
 def show_message():
@@ -29,8 +51,9 @@ def on_ctrl_pressed():
     global last_ctrl_press_time
     current_time = time.time()
     if current_time - last_ctrl_press_time < 0.5:  # Двойное нажатие в течение 0.5 секунд
-        show_message_thread = threading.Thread(target=show_message)
-        show_message_thread.start()
+        #show_message_thread = threading.Thread(target=show_message)
+        #show_message_thread.start()
+        get_selected_text()
     last_ctrl_press_time = current_time
 
 last_ctrl_press_time = 0
